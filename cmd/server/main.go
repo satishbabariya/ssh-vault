9428b9d6431ed950e59a171b2a328e7b4fab641b
Satish Babariya <satish.babariya@gmail.com>
2022-04-08 17:58:19 +0530

    add cred store

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"ssh-vault/internal/model"
+	"ssh-vault/internal/store"
 
-	"github.com/auth0/go-jwt-middleware/v2/validator"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
 	// auth0 "github.com/satishbabariya/go-echo-auth0-middleware"
 )
+
+func init() {
+	godotenv.Load()
+
+	if os.Getenv("AUTH0_ISSUER") == "" {
+		logrus.Fatal("AUTH0_ISSUER is not set")
+	}
+
+	if os.Getenv("AUTH0_AUDIENCE") == "" {
+		logrus.Fatal("AUTH0_AUDIENCE is not set")
+	}
+
+	if os.Getenv("VAULT_SECRET") == "" {
+		logrus.Fatal("VAULT_SECRET is not set")
+	}
+}
 
 func main() {
 	e := echo.New()
 	e.HideBanner = true
 
-	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// custom validator
+	e.Validator = model.NewValidator()
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${time_rfc3339} | ${status} | ${method} | ${latency_human} | ${uri}\n",
+		Output: logrus.New().Out,
+	}))
+
 	// e.Use(auth0.Auth0WithConfig(auth0.Auth0Config{
-	// 	Issuer:   "https://<your tenant domain>/",
-	// 	Audience: []string{"<your api identifier>"},
+	// 	Issuer:   os.Getenv("AUTH0_ISSUER"),
+	// 	Audience: []string{os.Getenv("AUTH0_AUDIENCE")},
 	// }))
 
 	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
@@ -28,10 +56,40 @@ func main() {
 		HTML5:  true,
 	}))
 
+	store, err := store.Open("./vault.db")
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	defer store.Close()
+
 	// Routes
-	e.GET("/api/validate", func(c echo.Context) error {
-		claims := c.Get("claims").(*validator.ValidatedClaims)
-		return c.JSON(http.StatusOK, claims)
+	// e.GET("/api/keys", func(c echo.Context) error {
+	// 	claims := c.Get("claims").(*validator.ValidatedClaims)
+	// 	return c.JSON(http.StatusOK, claims)
+	// })
+
+	e.GET("/api/credentials/:host", func(c echo.Context) error {
+		credential, err := store.Get(c.Param("host"))
+		if err != nil {
+			return c.JSON(http.StatusNotFound, err)
+		}
+		return c.JSON(http.StatusOK, credential)
+	})
+
+	e.POST("/api/credentials", func(c echo.Context) error {
+		cred := new(model.Credential)
+		if err = c.Bind(cred); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+		if err = c.Validate(cred); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+
+		if err = store.Add(*cred); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusCreated, fmt.Sprintf("Added %s", cred.Host))
 	})
 
 	// Start server
--- go.mod
+++ go.mod
@@ -3,24 +3,30 @@ module ssh-vault
 go 1.18
 
 require (
-	github.com/auth0/go-jwt-middleware/v2 v2.0.1
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/joho/godotenv v1.4.0
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.7.2
-	github.com/satishbabariya/go-echo-auth0-middleware v0.0.0-20211219165253-f2ce67824d82
+	github.com/sirupsen/logrus v1.8.1
+	go.etcd.io/bbolt v1.3.6
 	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 )
 
 require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
-	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
